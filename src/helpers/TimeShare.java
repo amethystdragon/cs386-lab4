849cb735500045bd68dcd7f5d73c7bf7c8340505
ggzaery@gmail.com <ggzaery@gmail.com@527977ea-3233-d4c1-4e3e-efd91543c65b>
2012-10-05 04:08:49 +0000

    Updated helpers

--- src/helpers/TimeShare.java
+++ src/helpers/TimeShare.java
@@ -1,55 +1,88 @@
 package helpers;
 
-public class TimeShre {
+public class TimeShare {
 
 	private String firstName;
 	private String lastName;
 	private String unitName;
 	private String unitNumber;
-	private String week;
-	
-	
-	public TimeShre(String firstName, String lastName, String unitName,
-			String unitNumber, String week) {
+	private int week;
+	private Customer customer;
+	private Unit unit;
+
+	public TimeShare(String firstName, String lastName, String unitName,
+			String unitNumber, int week) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.unitName = unitName;
 		this.unitNumber = unitNumber;
 		this.week = week;
 	}
+
+	public TimeShare(Customer customer, Unit unit, int week) {
+		this.setCustomer(customer);
+		this.setUnit(unit);
+		this.firstName = customer.getFirstName();
+		this.lastName = customer.getLastName();
+		this.unitName = unit.getUnitName();
+		this.unitNumber = unit.getUnitNumber();
+		this.week = week;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getUnitName() {
 		return unitName;
 	}
+
 	public void setUnitName(String unitName) {
 		this.unitName = unitName;
 	}
+
 	public String getUnitNumber() {
 		return unitNumber;
 	}
+
 	public void setUnitNumber(String unitNumber) {
 		this.unitNumber = unitNumber;
 	}
-	public String getWeek() {
+
+	public int getWeek() {
 		return week;
 	}
-	public void setWeek(String week) {
+
+	public void setWeek(int week) {
 		this.week = week;
 	}
-	
-	
-	
-	
-	
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
 }
